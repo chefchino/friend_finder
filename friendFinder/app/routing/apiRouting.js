0923121cf06a6d97efd148c921fb51fff8504259
Ciprien Bossut <chefchino@live.com>
2019-12-09 21:06:28 -0500

    got logic just need to add to survey.html the match and we are good

--- friendFinder/app/routing/apiRouting.js
+++ friendFinder/app/routing/apiRouting.js
@@ -3,22 +3,26 @@ var friendArray = require("../data/friends");
 module.exports = function(app) {
     app.post("/api/friends", function(req, res) {
         console.log("i'm in")
-        var newInput = req.body
-        var matchName = "";
-        var matchPic = "";
-        var difference = 100;
+        var newFriend = req.body;
+        var newInput = req.body.scores;
+        var scoreArray = [];
+        var match = 0;
         for (i = 0; i < friendArray.length; i++) {
             var compare = 0;
             for (j = 0; j < newInput.length; j++) {
-                compare += Math.abs(friendArray[i].scores - newInput[j]);
+            compare += (Math.abs(parseInt(friendArray[i].scores[j] - parseInt(newInput[j]))))
             }
-            if (compare < difference) {
-                difference = compare
-                matchName = friendArray[i].friendName;
-                matchPic = friendArray [i].linkToPic;
+            scoreArray.push(compare);
+            console.log("compare: " + compare)
+        }
+        for (var i = 0; i < scoreArray.length; i++) {
+            if (scoreArray[i] <= scoreArray[match]) {
+                match = i;
             }
         }
-        friendArray.push(newInput);
+        var buddy = friendArray[match];
+        console.log(buddy);
+        friendArray.push(newFriend);
         res.send("we hit the route")
     });
     app.get("/api/friends", function(req, res) {
